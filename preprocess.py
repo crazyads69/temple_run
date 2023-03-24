d75fb3d395956e75c51bed3fe11333dc8148b312
crazyads69 <leminhtri11a1@gmail.com>
2023-03-24 08:47:19 +0700

    Remove some function

--- preprocess.py
+++ preprocess.py
@@ -1,34 +1,10 @@
 import polars as pl
 from os import listdir
-from pyvi import ViTokenizer
 from os.path import isfile, join
 import string
 import emoji
 import csv
 import re
-
-"""
-    Create a function that remove non-Vietnamese word in sentences of dataset
-"""
-
-
-def remove_non_vietnamese_word(text):
-    """
-        Split sentence to word (aka token)
-    """
-    tokens = []
-    tokens = ViTokenizer.tokenize(text)
-    # Define a list of Vietnamese characters
-    vietnamese_chars = 'aăâbcdđeêghiklmnoôơpqrstuưvxy'
-
-    # Remove non-Vietnamese words from the list of tokens
-    vietnamese_tokens = [token for token in tokens if all(
-        char in vietnamese_chars for char in token)]
-
-    # Join the Vietnamese tokens back into a string
-    vietnamese_text = ' '.join(vietnamese_tokens)
-
-    return vietnamese_text
 
 
 """
@@ -84,7 +60,7 @@ def prepare_train_set():
         x = re.sub(r'\bcolon\w+\b', '', x)
         x = re.sub(r'\s+', ' ', x)
         x = x.lower()
-        x = remove_non_vietnamese_word(x)
+        x = re.sub(r'\bwzjwz\w+\b', '', x)
         train_data.append(x.translate(translator))
     print("Finish prepair train set")
     return train_data
@@ -113,7 +89,7 @@ def prepare_val_set():
         x = re.sub(r'\bcolon\w+\b', '', x)
         x = re.sub(r'\s+', ' ', x)
         x = x.lower()
-        x = remove_non_vietnamese_word(x)
+        x = re.sub(r'\bwzjwz\w+\b', '', x)
         val_data.append(x.translate(translator))
     print("Finish prepair val set")
     return val_data
@@ -142,8 +118,7 @@ def prepare_test_set():
         x = re.sub(r'\bcolon\w+\b', '', x)
         x = re.sub(r'\s+', ' ', x)
         x = x.lower()
-        x = remove_non_vietnamese_word(x)
+        x = re.sub(r'\bwzjwz\w+\b', '', x)
         test_data.append(x.translate(translator))
-    print(test_data)
     print("Finish prepair test set")
     return test_data
