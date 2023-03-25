8126ca4924685eadddd3cb454f0948e2bb5fa0a3
crazyads69 <leminhtri11a1@gmail.com>
2023-03-25 10:28:35 +0700

    Correct model

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import accuracy_score
 import torch.nn as nn
 from preprocess import *
 import torch
@@ -51,11 +52,11 @@ val_dataset = SentenceDataset(val_data, val_label, tokenizer)
 test_dataset = SentenceDataset(test_data, test_label, tokenizer)
 
 train_dataloader = DataLoader(
-    train_dataset, batch_size=32, shuffle=True)
+    train_dataset, batch_size=64, shuffle=True)
 val_dataloader = DataLoader(
-    val_dataset, batch_size=32, shuffle=False)
+    val_dataset, batch_size=64, shuffle=False)
 test_dataloader = DataLoader(
-    test_dataset, batch_size=32, shuffle=False)
+    test_dataset, batch_size=64, shuffle=False)
 
 
 class BiLSTMModel(pl.LightningModule):
@@ -65,20 +66,22 @@ class BiLSTMModel(pl.LightningModule):
         self.bilstm = nn.LSTM(embedding_dim, hidden_dim,
                               num_layers=num_layers, batch_first=True, bidirectional=True)
         self.dropout = nn.Dropout(dropout_prob)
-        self.attention = nn.Linear(hidden_dim * 2, 1)
         self.fc1 = nn.Linear(hidden_dim * 2, hidden_dim)
-
+        self.attention = nn.Linear(hidden_dim, 1)
         self.fc2 = nn.Linear(hidden_dim, 1)
+        self.training_step_outputs = []
+        self.validation_step_outputs = []
+        self.test_step_outputs = []
 
     def forward(self, input_ids, attention_mask):
         embedded = self.embedding(input_ids)
         embedded = self.dropout(embedded)
         outputs, _ = self.bilstm(embedded)
         outputs = self.dropout(outputs)
-        attention_weights = F.softmax(self.attention(outputs), dim=1)
+        dense_outputs = F.relu(self.fc1(outputs))
+        attention_weights = F.softmax(self.attention(dense_outputs), dim=1)
         weighted_outputs = torch.sum(outputs * attention_weights, dim=1)
-        dense_outputs = F.relu(self.fc1(weighted_outputs))
-        logits = self.fc2(dense_outputs)
+        logits = self.fc2(weighted_outputs)
         return logits.squeeze()
 
     def training_step(self, batch, batch_idx):
@@ -86,44 +89,96 @@ class BiLSTMModel(pl.LightningModule):
         attention_mask = batch['attention_mask']
         labels = batch['label']
         logits = self(input_ids, attention_mask)
-        preds = torch.round(torch.sigmoid(logits))
         loss = F.binary_cross_entropy_with_logits(logits, labels)
-        acc = (preds == labels).float().mean()
         self.log('train_loss', loss, prog_bar=True,
                  on_step=True, on_epoch=True)
-        self.log('train_acc', acc, prog_bar=True, on_step=True, on_epoch=True)
-        return {'loss': loss, 'acc': acc}
+        self.training_step_outputs.append(
+            {'loss': loss, 'label': labels, 'logits': logits})
+        return loss
+
+    def on_train_epoch_end(self):
+        train_loss = torch.stack([x['loss']
+                                 for x in self.training_step_outputs]).mean()
+        self.log('train_loss_epoch', train_loss, prog_bar=True, on_epoch=True)
+        y_true = []
+        y_pred = []
+        for output in self.training_step_outputs:
+            if 'label' in output:
+                y_true.append(output['label'].item())
+            y_pred.append(torch.sigmoid(output['logits']).round().item())
+        acc = accuracy_score(y_true, y_pred)
+        self.log('train_acc', acc, prog_bar=True, on_epoch=True)
+        self.training_step_outputs.clear()
 
     def validation_step(self, batch, batch_idx):
         input_ids = batch['input_ids']
         attention_mask = batch['attention_mask']
         labels = batch['label']
         logits = self(input_ids, attention_mask)
-        preds = torch.round(torch.sigmoid(logits))
-        acc = (preds == labels).float().mean()
         loss = F.binary_cross_entropy_with_logits(
             logits, labels)
         self.log('val_loss', loss, prog_bar=True, on_step=True, on_epoch=True)
-        self.log('val_acc', acc, prog_bar=True, on_step=True, on_epoch=True)
-        return {'loss': loss, 'acc': acc}
+        self.validation_step_outputs.append(
+            {'loss': loss, 'label': labels, 'logits': logits})
+        return loss
+
+    def on_validation_epoch_end(self):
+        val_loss = torch.stack([x['loss']
+                                for x in self.validation_step_outputs]).mean()
+        self.log('val_loss_epoch', val_loss, prog_bar=True, on_epoch=True)
+        y_true = []
+        y_pred = []
+        for output in self.validation_step_outputs:
+            if 'label' in output:
+                y_true.append(output['label'].item())
+            y_pred.append(torch.sigmoid(output['logits']).round().item())
+        acc = accuracy_score(y_true, y_pred)
+        self.log('val_acc', acc, prog_bar=True, on_epoch=True)
+        self.validation_step_outputs.clear()
+
+    def test_step(self, batch, batch_idx):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        labels = batch['label']
+        logits = self(input_ids, attention_mask)
+        loss = F.binary_cross_entropy_with_logits(
+            logits, labels)
+        self.log('test_loss', loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.test_step_outputs.append(
+            {'loss': loss, 'label': labels, 'logits': logits})
+        return loss
+
+    def on_test_epoch_end(self):
+        test_loss = torch.stack([x['loss']
+                                for x in self.test_step_outputs]).mean()
+        self.log('test_loss_epoch', test_loss, prog_bar=True, on_epoch=True)
+        y_true = []
+        y_pred = []
+        for output in self.test_step_outputs:
+            if 'label' in output:
+                y_true.append(output['label'].item())
+            y_pred.append(torch.sigmoid(output['logits']).round().item())
+        acc = accuracy_score(y_true, y_pred)
+        self.log('test_acc', acc, prog_bar=True, on_epoch=True)
+        self.test_step_outputs.clear()
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer, mode='min', factor=0.5, patience=1)
         return {'optimizer': optimizer, 'lr_scheduler': scheduler, 'monitor': 'val_loss'}
 
 
 vocab_size = tokenizer.vocab_size
-embedding_dim = 128
-hidden_dim = 64
-num_layers = 2
+embedding_dim = 256
+hidden_dim = 128
+num_layers = 4
 dropout_prob = 0.2
 model = BiLSTMModel(vocab_size, embedding_dim,
                     hidden_dim, num_layers, dropout_prob)
 
-trainer = pl.Trainer(max_epochs=20, reload_dataloaders_every_n_epochs=1,
+trainer = pl.Trainer(max_epochs=50, reload_dataloaders_every_n_epochs=1,
                      enable_checkpointing=1, enable_progress_bar=1, detect_anomaly=True)
 trainer.fit(model, train_dataset, val_dataset)
 trainer.save_checkpoint('checkpoint.ckpt')
-trainer.test(model, test_dataset)
+trainer.test(model, 'checkpoint.ckpt', test_dataset)
